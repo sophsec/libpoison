aefa5fc0d4c46d427078777884fb4f7510e2ce18
drraid <drraid@gmail.com>
2009-05-07 02:33:12 -0700

    dhcp base code

---
+++ src/poison_dhcp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <libnet.h>
+
+int poison_add_dhcp(poison_session_t *session, poison_dhcp_strategy_t strategy, poison_target_t *target)
+{
+	
+
+}
+
