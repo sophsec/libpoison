5cfbd962253785b1e7009aba2b5117aa6bcc0443
drraid <drraid@gmail.com>
2009-05-07 03:03:18 -0700

    Removed libnet.h, included libpoison.h which now includes libnet.h

--- src/poison_dhcp.c
+++ src/poison_dhcp.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#include <libnet.h>
+#include "libpoison.h"
 
 /* this function registers a new target for dhcp poisoning */
 int poison_add_dhcp(poison_session_t *session, poison_target_t *target, dhcp_strategy_t)
