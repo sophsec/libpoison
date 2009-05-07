0e2ed213b3e9f1bcce739502b79f4dd3b895d52a
postmodern <postmodern.mod3@gmail.com>
2009-05-06 18:32:43 -0700

    Use ip_addr_t.

--- src/target.h
+++ src/target.h
@@ -1,12 +1,14 @@
 #ifndef _LIBPOISON_TARGET_H_
 #define _LIBPOISON_TARGET_H_
 
+#include "types.h"
+
 struct poison_target;
 typedef struct poison_target poison_target_t;
 
 struct poison_target;
 {
-	uint32_t ip_addr;
+	ip_addr_t ip_addr;
 
 	poison_target_t *next;
 };
