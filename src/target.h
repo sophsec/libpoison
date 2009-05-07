7fc8c5e73bc95feedea5bc975bc58709117739d6
postmodern <postmodern.mod3@gmail.com>
2009-05-06 18:25:32 -0700

    Added a next pointer.

--- src/target.h
+++ src/target.h
@@ -1,9 +1,14 @@
 #ifndef _LIBPOISON_TARGET_H_
 #define _LIBPOISON_TARGET_H_
 
-typedef struct
+struct poison_target;
+typedef struct poison_target poison_target_t;
+
+struct poison_target;
 {
 	uint32_t ip_addr;
-} poison_target_t;
+
+	poison_target_t *next;
+};
 
 #endif
