89fd6ac23c748849cfa7fbf48332bb7fc9e1779c
postmodern <postmodern.mod3@gmail.com>
2009-05-21 21:04:36 -0700

    Added a libpoison.h file that will be installed into INCLUDE_DIR.

---
+++ libpoison.h
@@ -0,0 +1,10 @@
+#ifndef _LIBPOISON_H_
+#define _LIBPOISON_H_
+
+#include <libpoison/poison_types.h>
+#include <libpoison/poison_host.h>
+#include <libpoison/poison_target.h>
+#include <libpoison/poison_trigger.h>
+#include <libpoison/poison_session.h>
+
+#endif
