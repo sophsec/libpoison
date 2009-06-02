3c87d4ef984aa011156c55520202d9c969f918c2
drraid <drraid@gmail.com>
2009-06-01 17:10:46 -0700

    stub file added to satisfy compile

---
+++ src/poison_host.c
@@ -0,0 +1,15 @@
+#include "poison_host.h"
+#include "libpoison.h"
+
+/* if 0 for now: to get other files compiling 
+	until this is ready to be worked on  */
+#if 0
+
+void poison_hoststub(void)
+{
+
+}
+
+#endif
+
+
