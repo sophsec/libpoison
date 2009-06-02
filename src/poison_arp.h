ff87547d6d343127a3394a37325d59266a95569e
postmodern <postmodern.mod3@gmail.com>
2009-06-01 17:07:00 -0700

    Include "poison_session.h".

--- src/poison_arp.h
+++ src/poison_arp.h
@@ -2,6 +2,7 @@
 #define _LIBPOISON_ARP_H_
 
 #include "poison_types.h"
+#include "poison_session.h"
 
 int poison_arp(poison_session_t *session, unsigned short op, unsigned char *destMAC, ip_addr_t destIP, unsigned char *srcMAC, ip_addr_t srcIP);
 
