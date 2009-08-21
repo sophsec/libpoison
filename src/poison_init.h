ad96b7701ae9dabfdbee5fe1a21d5c793f9b613e
postmodern <postmodern.mod3@gmail.com>
2009-08-20 22:34:54 -0700

    Added function prototypes to poison_init.h.

--- src/poison_init.h
+++ src/poison_init.h
@@ -3,4 +3,10 @@
 
 #define SESSION_INITIALIZED 77
 
+#include "poison_session.h"
+
+int poison_init(poison_session_t *session, char *interface);
+int poison_check_init(poison_session_t *session);
+void poison_shutdown(poison_session_t *session);
+
 #endif
